d6dbe9a7d32864590bcc957c122d8446445a3980
ankita <ankita@DESKTOP-EF8E6FR>
2020-06-05 21:09:07 +0530

    Added two java class file

---
+++ GitTutorial/src/Calculation.java
@@ -0,0 +1,19 @@
+
+public class Calculation {
+	
+	public void sum(int a, int b)
+	{
+		int add = a+b;
+		System.out.println("addition of two integer variables:" + add);
+		
+	}
+
+	public void sub(int a,  int b)
+	{
+		int sub = a-b;
+		
+		System.out.println("Substraction of two integer:" + sub);
+	}
+
+	
+}
---
+++ GitTutorial/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculation c = new Calculation();
+		c.sum(10, 20);
+		c.sub(20, 10);
+	}
+
+}
