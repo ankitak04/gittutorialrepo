495f2fbf723747eb2f350a0982fba462e6847ac6
ankita <learningautomation@gmail.com>
2020-06-06 12:30:39 +0530

    Deleted LocalOP-No Use and mofidy in Employee file

--- GitTutorial/src/Employee.java
+++ GitTutorial/src/Employee.java
@@ -3,23 +3,25 @@ public class Employee {
 	
 	private int empid;
 	private String empname;
+	private int empsalary;
 	
-	public Employee(int empid, String empname)
+	public Employee(int empid, String empname , int empsalary)
 	{
 		this.empid= empid;
 		this.empname = empname;
+		this.empsalary = empsalary;
 	}
 	
 	
 
 	public void display()
 	{
-		System.out.println("EMp id & Emp Name :" +empid + " >> " +empname);
+		System.out.println("EMp id & Emp Name :" +empid + " >> " +empname +">>>"+ empsalary);
 	}
 
 	public static void main(String[] args)
 	{
-		Employee e = new Employee(234,"Ankki");
+		Employee e = new Employee(234,"Ankki" , 20000);
 		e.display();
 	}
 }
