410a09e0acc2c17f1dd5156c7b1f72a00f8a67e4
ankita <learningautomation@gmail.com>
2020-06-06 09:29:03 +0530

    Partial implemented code.

--- GitTutorial/src/Employee.java
+++ GitTutorial/src/Employee.java
@@ -1,6 +1,20 @@
 
 public class Employee {
+	
+	private int empid;
+	
+	public Employee(int empid)
+	{
+		this.empid= empid;
+	}
 
-	
-	
+	public void display()
+	{
+		System.out.println("EMp id :" +empid);
+	}
+
+	public static void main(String[] args)
+	{
+		
+	}
 }
