627ef6b22f75340bb02824a07e17e511e5c3dc06
ankita <learningautomation@gmail.com>
2020-06-06 12:14:49 +0530

    Added *

--- GitTutorial/src/Sourcefile.java
+++ GitTutorial/src/Sourcefile.java
@@ -4,7 +4,11 @@ public class Sourcefile {
 	public static void main(String[] args) {
 		
 		System.out.println("sourcefile");
-
+		System.out.println("______________________________________");
+		System.out.println("*");
+		System.out.println("* *");
+		System.out.println("* * *");
+		System.out.println("_______________________________________");
 	}
 
 }
