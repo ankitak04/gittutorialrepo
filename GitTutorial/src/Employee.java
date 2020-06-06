d9d42e028991a503a547fea20045187a43a92205
ankita <learningautomation@gmail.com>
2020-06-06 09:34:23 +0530

    Added Empname info in code.

--- GitTutorial/src/Employee.java
+++ GitTutorial/src/Employee.java
@@ -2,20 +2,24 @@
 public class Employee {
 	
 	private int empid;
+	private String empname;
 	
-	public Employee(int empid)
+	public Employee(int empid, String empname)
 	{
 		this.empid= empid;
+		this.empname = empname;
 	}
+	
+	
 
 	public void display()
 	{
-		System.out.println("EMp id :" +empid);
+		System.out.println("EMp id & Emp Name :" +empid + " >> " +empname);
 	}
 
 	public static void main(String[] args)
 	{
-		Employee e = new Employee(234);
+		Employee e = new Employee(234,"Ankki");
 		e.display();
 	}
 }
