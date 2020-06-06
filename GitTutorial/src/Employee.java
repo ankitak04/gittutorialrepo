5262d53a3234df3db55df26c7ed0d001a47b5f7d
ankita <learningautomation@gmail.com>
2020-06-06 12:35:03 +0530

    Modify changes

--- GitTutorial/src/Employee.java
+++ GitTutorial/src/Employee.java
@@ -4,24 +4,26 @@ public class Employee {
 	private int empid;
 	private String empname;
 	private int empsalary;
+	private String empBankName;
 	
-	public Employee(int empid, String empname , int empsalary)
+	public Employee(int empid, String empname , int empsalary , String empBankName)
 	{
 		this.empid= empid;
 		this.empname = empname;
 		this.empsalary = empsalary;
+		this.empBankName = empBankName;
 	}
 	
 	
 
 	public void display()
 	{
-		System.out.println("EMp id & Emp Name :" +empid + " >> " +empname +">>>"+ empsalary);
+		System.out.println("EMp id & Emp Name :" +empid + " >> " +empname +">>>"+ empsalary +" "+empBankName);
 	}
 
 	public static void main(String[] args)
 	{
-		Employee e = new Employee(234,"Ankki" , 20000);
+		Employee e = new Employee(234,"Ankki" , 20000,"HDFC");
 		e.display();
 	}
 }
